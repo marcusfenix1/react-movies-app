f39e19e8e344314ddf58303aad3404cfc98eca49
marcusfenix1 <48603513+marcusfenix1@users.noreply.github.com>
2020-03-02 22:58:37 +0200

    fetch function fixed v2

--- src/services/moviesApi.js
+++ src/services/moviesApi.js
@@ -3,16 +3,26 @@ import axios from "axios";
 const apiKey = "628d6a5d740bdc44977e233b871867be";
 const baseUrl = "https://api.themoviedb.org/3/";
 
-const fetchMovieDetails = async movieId => {
+// const fetchMovieDetails = async movieId => {
+//   const requestParams = `movie/${movieId}?api_key=${apiKey}`;
+//   try {
+//     const response = await axios.get(baseUrl + requestParams);
+//     const data = response.data;
+//     return data;
+//   } catch (error) {
+//     return console.log(error);
+//   }
+// };
+
+const fetchMovieDetails = movieId => {
   const requestParams = `movie/${movieId}?api_key=${apiKey}`;
-  try {
-    const response = await axios.get(baseUrl + requestParams);
-    const data = response.data;
-    return data;
-  } catch (error) {
-    return console.log(error);
-  }
+  return axios.get(baseUrl + requestParams).then(response => response.data);
 };
+
+// const fetchMovieDetails = movieId => {
+//   const requestParams = `movie/${movieId}?api_key=${apiKey}`;
+//   return fetch(baseUrl + requestParams).then(res => res.json());
+// };
 
 // const fetchMovieWithQuery = searchQuery => {
 //   return fetch(`${baseURL}/search/shows?q=${searchQuery}`)
