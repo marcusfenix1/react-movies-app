15ea0467d4f1b09c25d23ecaaeaa9f9549df8a77
marcusfenix1 <48603513+marcusfenix1@users.noreply.github.com>
2020-03-10 21:59:52 +0200

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,29 @@ import React, { Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import routes from "../routes";
+import Layout from "../components/Layout";
+import HomePage from "../pages/HomePage";
+import MoviesDetailsPage from "../pages/MoviesDetailsPage";
+import MoviesPage from "../pages/MoviesPage";
 
-const Layout = React.lazy(() => import("./Layout"));
-const HomePage = React.lazy(() => import("../pages/HomePage"));
-const MoviesDetailsPage = React.lazy(() =>
-  import("../pages/MoviesDetailsPage")
-);
-const MoviesPage = React.lazy(() => import("../pages/MoviesPage"));
+// const Layout = React.lazy(() => import("./Layout"));
+// const HomePage = React.lazy(() => import("../pages/HomePage"));
+// const MoviesDetailsPage = React.lazy(() =>
+//   import("../pages/MoviesDetailsPage")
+// );
+// const MoviesPage = React.lazy(() => import("../pages/MoviesPage"));
 
 const App = () => (
-  <Suspense fallback={<Spinner />}>
-    <Layout>
-      <Switch>
-        <Route path={routes.home} exact component={HomePage} />
-        <Route path={routes.movies} exact component={MoviesPage} />
-        <Route path={routes.movieDetails} component={MoviesDetailsPage} />
-        <Redirect to={routes.home} />
-      </Switch>
-    </Layout>
-  </Suspense>
+  // <Suspense fallback={<Spinner />}>
+  <Layout>
+    <Switch>
+      <Route path={routes.home} exact component={HomePage} />
+      <Route path={routes.movies} exact component={MoviesPage} />
+      <Route path={routes.movieDetails} component={MoviesDetailsPage} />
+      <Redirect to={routes.home} />
+    </Switch>
+  </Layout>
+  // </Suspense>
 );
 
 export default App;
